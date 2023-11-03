76f03d54ce04256285f78bfb4f4b660cfb0c108c
mahaj1 <134968479+mahaj1@users.noreply.github.com>
2023-11-03 14:10:21 -0500

    last assignment code

---
+++ Lab4/src/main/java/com/example/lab4/Service/UserService.java
@@ -0,0 +1,11 @@
+package com.example.lab4.Service;
+
+import com.example.lab4.Model.User;
+
+public interface UserService {
+    User findById(int id);
+    User saveUser(User user);
+    User updateUser(int id,User user);
+    void deleteUser(int id);
+
+}
